20520ac5568ddddf2306d549b986cc1198addef4
felix <adfelix2@gmail.com>
2017-12-26 18:45:30 -0600

    Added All Assets

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ var file = new static.Server();
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
     file.serve(request, response);
+    console.log("Go to localhost:3000");
   }).resume();
 }).listen(process.env.PORT || 3000);
